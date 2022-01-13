0ff5e062456e5f89735c16a24a6175542e8a1afc
Evian-Zhang <evianzhang1999@163.com>
2022-01-13 16:52:35 +0800

    Complete coding stuff

--- err-per-field-derive/src/lib.rs
+++ err-per-field-derive/src/lib.rs
@@ -2,11 +2,11 @@ use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 use quote::quote;
 use syn::{
-    parse_macro_input, spanned::Spanned, Attribute, Data, DeriveInput, Error, Fields, Lit, Meta,
-    MetaNameValue, NestedMeta, Path,
+    parse_macro_input, spanned::Spanned, Attribute, Data, DeriveInput, Error, Field, Fields, Lit,
+    Meta, NestedMeta, Path,
 };
 
-fn process_meta_name_value<F: FnMut(&MetaNameValue)>(attrs: &Vec<Attribute>, f: &mut F) {
+fn process_submeta<F: FnMut(&Meta)>(attrs: &Vec<Attribute>, f: &mut F) {
     for attr in attrs {
         let list = if let Ok(Meta::List(list)) = attr.parse_meta() {
             list
@@ -27,14 +27,15 @@ fn process_meta_name_value<F: FnMut(&MetaNameValue)>(attrs: &Vec<Attribute>, f: 
             } else {
                 continue;
             };
-            let meta_name_value = if let Meta::NameValue(meta_name_value) = submeta {
-                meta_name_value
-            } else {
-                continue;
-            };
-            f(meta_name_value);
+            f(submeta);
         }
     }
+}
+
+enum FieldWrapper {
+    Result(Path),
+    Option,
+    Raw,
 }
 
 #[proc_macro_derive(ErrPerField, attributes(err_per_field))]
@@ -44,11 +45,11 @@ pub fn err_per_field(input: TokenStream) -> TokenStream {
     let mut compiler_errors = vec![];
 
     let DeriveInput {
-        attrs: container_attrs,
         vis: container_vis,
         ident: container_ident,
         generics: container_generics,
         data: container_data,
+        ..
     } = &input;
 
     // Build wrapper struct name
@@ -60,123 +61,141 @@ pub fn err_per_field(input: TokenStream) -> TokenStream {
     // Build wrapper struct generics
     let (impl_generics, ty_generics, where_clause) = container_generics.split_for_impl();
 
-    // Get top-level wrapper
-    let mut error_path = None;
-    process_meta_name_value(container_attrs, &mut |meta_name_value| {
-        let ident = if let Some(ident) = meta_name_value.path.get_ident() {
-            ident
-        } else {
-            return;
-        };
-        if ident != "error" {
-            compiler_errors.push(
-                Error::new(ident.span(), &format!("Unknown key {}", ident)).to_compile_error(),
-            );
-            return;
-        }
-        let str_lit = if let Lit::Str(str_lit) = &meta_name_value.lit {
-            str_lit
-        } else {
-            compiler_errors.push(
-                Error::new(meta_name_value.lit.span(), "Unsupported value type").to_compile_error(),
-            );
-            return;
-        };
-        let path = if let Ok(path) = syn::parse_str::<Path>(&str_lit.value()) {
-            path
-        } else {
-            compiler_errors.push(
-                Error::new(
-                    meta_name_value.lit.span(),
-                    &format!("Unable to convert {} to a valid path.", str_lit.value()),
-                )
-                .to_compile_error(),
-            );
-            return;
-        };
-        error_path = Some(path);
-    });
-
-    let top_level_wrapper = if let Some(error_path) = error_path {
-        quote! { ::core::result::Result<#container_ident #ty_generics, #error_path> }
+    // Iterate struct body
+    let struct_data = if let Data::Struct(struct_data) = container_data {
+        struct_data
     } else {
-        quote! { ::core::option::Option<#container_ident> }
+        return TokenStream::from(
+            Error::new(input.span(), "Unsupported data type").to_compile_error(),
+        );
     };
-
-    // Build wrapper struct body
-    // let mut wrapper_body = vec![];
-    match container_data {
-        Data::Struct(struct_data) => match &struct_data.fields {
-            Fields::Named(fields) => {
-                for field in &fields.named {
-                    let field_attrs = &field.attrs;
-                    let field_name = &field.ident;
-                    let field_ty = &field.ty;
-                    process_meta_name_value(field_attrs, &mut |meta_name_value| {
-                        let ident = if let Some(ident) = meta_name_value.path.get_ident() {
-                            ident
-                        } else {
-                            return;
-                        };
-                        match ident.to_string().as_ref() {
-                            "error" => {}
-                            "skip_error" => {}
-                            _ => {
-                                compiler_errors.push(
-                                    Error::new(ident.span(), &format!("Unknown key {}", ident))
-                                        .to_compile_error(),
-                                );
-                                return;
-                            }
-                        }
-                        let str_lit = if let Lit::Str(str_lit) = &meta_name_value.lit {
-                            str_lit
-                        } else {
-                            compiler_errors.push(
-                                Error::new(meta_name_value.lit.span(), "Unsupported value type")
-                                    .to_compile_error(),
-                            );
-                            return;
-                        };
-                        let path = if let Ok(path) = syn::parse_str::<Path>(&str_lit.value()) {
-                            path
-                        } else {
-                            compiler_errors.push(
-                                Error::new(
-                                    meta_name_value.lit.span(),
-                                    &format!(
-                                        "Unable to convert {} to a valid path.",
-                                        str_lit.value()
-                                    ),
-                                )
-                                .to_compile_error(),
-                            );
-                            return;
-                        };
-                    });
+    let fields = if let Fields::Named(fields) = &struct_data.fields {
+        fields
+    } else {
+        return TokenStream::from(
+            Error::new(input.span(), "Unsupported data type").to_compile_error(),
+        );
+    };
+    let mut wrapper_body = vec![];
+    let mut inner_body = vec![];
+    let mut matched_field_patterns = vec![];
+    for field in &fields.named {
+        let Field {
+            attrs: field_attrs,
+            vis: field_vis,
+            ident: field_name,
+            ty: field_ty,
+            ..
+        } = &field;
+        inner_body.push(field_name);
+        let mut field_wrapper = FieldWrapper::Raw;
+        process_submeta(field_attrs, &mut |submeta| match submeta {
+            Meta::Path(meta_path) => {
+                let ident = if let Some(ident) = meta_path.get_ident() {
+                    ident
+                } else {
+                    compiler_errors
+                        .push(Error::new(meta_path.span(), "Unknown key").to_compile_error());
+                    return;
+                };
+                if ident != "maybe_none" {
+                    compiler_errors.push(
+                        Error::new(ident.span(), &format!("Unknown key {}", ident))
+                            .to_compile_error(),
+                    );
+                    return;
                 }
+                field_wrapper = FieldWrapper::Option;
+            }
+            Meta::NameValue(meta_name_value) => {
+                let ident = if let Some(ident) = meta_name_value.path.get_ident() {
+                    ident
+                } else {
+                    return;
+                };
+                if ident != "maybe_error" {
+                    compiler_errors.push(
+                        Error::new(ident.span(), &format!("Unknown key {}", ident))
+                            .to_compile_error(),
+                    );
+                    return;
+                }
+                let str_lit = if let Lit::Str(str_lit) = &meta_name_value.lit {
+                    str_lit
+                } else {
+                    compiler_errors.push(
+                        Error::new(meta_name_value.lit.span(), "Unsupported value type")
+                            .to_compile_error(),
+                    );
+                    return;
+                };
+                let path = if let Ok(path) = syn::parse_str::<Path>(&str_lit.value()) {
+                    path
+                } else {
+                    compiler_errors.push(
+                        Error::new(
+                            meta_name_value.lit.span(),
+                            &format!("Unable to convert {} to a valid path.", str_lit.value()),
+                        )
+                        .to_compile_error(),
+                    );
+                    return;
+                };
+                field_wrapper = FieldWrapper::Result(path);
             }
             _ => {
-                return TokenStream::from(
-                    Error::new(input.span(), "Unsupported data type").to_compile_error(),
-                )
+                return;
             }
-        },
-        _ => {
-            return TokenStream::from(
-                Error::new(input.span(), "Unsupported data type").to_compile_error(),
-            )
-        }
+        });
+        let wrapper_body_part = match &field_wrapper {
+            FieldWrapper::Raw => {
+                quote! { #field_vis #field_name: #field_ty }
+            }
+            FieldWrapper::Option => {
+                quote! { #field_vis #field_name: ::core::option::Option<#field_ty> }
+            }
+            FieldWrapper::Result(path) => {
+                quote! { #field_vis #field_name: ::core::result::Result<#field_ty, #path> }
+            }
+        };
+        let matched_field_pattern = match &field_wrapper {
+            FieldWrapper::Raw => {
+                quote! { #field_name }
+            }
+            FieldWrapper::Option => {
+                quote! { #field_name: Some(#field_name) }
+            }
+            FieldWrapper::Result(_) => {
+                quote! { #field_name: Ok(#field_name) }
+            }
+        };
+        wrapper_body.push(wrapper_body_part);
+        matched_field_patterns.push(matched_field_pattern);
     }
 
     let expanded = quote! {
         #(#compiler_errors)*
-        #container_vis #wrapper_struct_ident #container_generics {
-            data: #top_level_wrapper,
+        #container_vis struct #wrapper_struct_ident #container_generics {
+            #(#wrapper_body),*
         }
 
-        impl #impl_generics #wrapper_struct_ident #ty_generics #where_clause {
+        impl #impl_generics err_per_field::FieldMayErr for #container_ident #ty_generics #where_clause {
+            type Wrapper = #wrapper_struct_ident #ty_generics;
+        }
 
+        impl #impl_generics ::core::convert::TryFrom<#wrapper_struct_ident #ty_generics> for #container_ident #ty_generics #where_clause {
+            type Error = #wrapper_struct_ident #ty_generics;
+            fn try_from(value: #wrapper_struct_ident #ty_generics) -> Result<Self, Self::Error> {
+                if let #wrapper_struct_ident #ty_generics {
+                    #(#matched_field_patterns),*
+                } = value {
+                    Ok(#container_ident {
+                        #(#inner_body),*
+                    })
+                } else {
+                    Err(value)
+                }
+            }
         }
     };
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,59 @@
+//! ```rust
+//! use err_per_field::{ErrPerField, Wrapper};
+//!
+//! struct AnError;
+//!
+//! #[derive(ErrPerField)]
+//! struct Foo {
+//!     pub bar1: u8,
+//!     #[err_per_field(maybe_none)]
+//!     pub bar2: u16,
+//!     #[err_per_field(maybe_error = "AnError")]
+//!     pub bar3: u32,
+//!     pub bar4: u64,
+//! }
+//!
+//! fn baz1() -> u8 { 0 }
+//! fn baz2() -> Option<u16> { None }
+//! fn baz3() -> Result<u32, AnError> { Err(AnError) }
+//! fn baz4() -> u64 { 0 }
+//!
+//! fn generate_foo() -> Wrapper<Foo> {
+//!     let bar1: u8 = baz1();
+//!     let bar2: Option<u16> = baz2();
+//!     let bar3: Result<u32, AnError> = baz3();
+//!     let bar4: u64 = baz4();
+//!     Wrapper::<Foo> {
+//!         bar1,
+//!         bar2,
+//!         bar3,
+//!         bar4,
+//!     }
+//! }
+//!
+//! let result: Result<Foo, Wrapper<Foo>> = generate_foo().try_into();
+//! assert!(result.is_err());
+//! match result {
+//!     Ok(foo) => {
+//!         // foo is of type Foo, and you can directly use foo without any worries
+//!     },
+//!     Err(foo_wrapper) => {
+//!         // foo_wrapper has the same fields as foo, but in different types
+//!         // such as foo_wrapper.bar2 is of type Option<u16> and
+//!         // foo_wrapper.bar3 is of type Result<u32, AnError>
+//!     }
+//! }
+//! ```
+
+/// Re-export of derived macro
 pub use err_per_field_derive::ErrPerField;
 
-/// Error trait used for using this crate.
-///
-/// If the marked struct can throw an error, then such error
-/// should implement this trait
-pub trait Error {
-    /// There is something wrong in the internal fields, but not
-    /// the whole struct
-    fn map_per_field_err() -> Self;
+/// Trait used for inner usage. DO NOT USE DIRECTLY!
+#[doc(hidden)]
+pub trait FieldMayErr {
+    /// Wrapper of this struct
+    type Wrapper;
 }
+
+/// A wrapper of the raw struct.
+pub type Wrapper<T> = <T as FieldMayErr>::Wrapper;
